160e2138fc192494703cf209b714c2f100bc08f0
max <progrmax@gmail.com>
2019-06-13 13:45:49 +0300

    fix namespace

--- src/AbstractFile.php
+++ src/AbstractFile.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Strrongknit\FileReader;
+namespace Strongknit\FileReader;
 
 use Iterator;
 
--- src/ExcelFile.php
+++ src/ExcelFile.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Strrongknit\FileReader;
+namespace Strongknit\FileReader;
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Settings;
